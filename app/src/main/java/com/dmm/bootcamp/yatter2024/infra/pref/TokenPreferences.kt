27e903c5126e8a5e758a1362ad66dfe2fa4dc972
miyazato-hayato1 <miyazato-hayato1@dmm.com>
2024-04-15 10:34:27 +0900

    トークンをアプリで保持できるようにPreferences実装

---
+++ app/src/main/java/com/dmm/bootcamp/yatter2024/infra/pref/TokenPreferences.kt
@@ -0,0 +1,29 @@
+package com.dmm.bootcamp.yatter2024.infra.pref
+
+import android.content.Context
+
+class TokenPreferences(private val context: Context) {
+  companion object {
+    private const val PREF_NAME = "token"
+    private const val KEY_ACCESS_TOKEN = "access_token"
+  }
+
+  private val sharedPreferences = context.getSharedPreferences(
+    PREF_NAME,
+    Context.MODE_PRIVATE
+  )
+
+  fun getAccessToken(): String? = sharedPreferences.getString(
+    KEY_ACCESS_TOKEN,
+    ""
+  )
+
+  fun putAccessToken(token: String?) {
+    sharedPreferences.edit().putString(
+      KEY_ACCESS_TOKEN,
+      token
+    ).apply()
+  }
+
+  fun clear() = sharedPreferences.edit().clear().apply()
+}
