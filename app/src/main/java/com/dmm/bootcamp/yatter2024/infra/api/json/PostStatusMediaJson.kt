ec997f2c6f13d32bd5d2b061c42f4bdf782f951e
miyazato-hayato1 <miyazato-hayato1@dmm.com>
2024-04-16 10:54:43 +0900

    Status投稿のAPIリクエストボディの修正

--- app/src/main/java/com/dmm/bootcamp/yatter2024/infra/api/json/PostStatusMediaJson.kt
+++ app/src/main/java/com/dmm/bootcamp/yatter2024/infra/api/json/PostStatusMediaJson.kt
@@ -4,7 +4,7 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class PostStatusJson(
-  val status: String,
-  @Json(name = "medias") val mediaList: List<PostStatusMediaJson>,
+data class PostStatusMediaJson(
+  @Json(name = "media_id") val mediaId: Int,
+  val description: String,
 )
