66aaca7d11deaf5159d20a2d064b48830e5471d0
miyazato-hayato1 <miyazato-hayato1@dmm.com>
2024-04-16 10:53:10 +0900

    Json用のデータクラスに必要なアノテーション追加

--- app/src/main/java/com/dmm/bootcamp/yatter2024/infra/api/json/MediaJson.kt
+++ app/src/main/java/com/dmm/bootcamp/yatter2024/infra/api/json/MediaJson.kt
@@ -1,7 +1,9 @@
 package com.dmm.bootcamp.yatter2024.infra.api.json
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class MediaJson(
   @Json(name = "id") val id: String,
   @Json(name = "type") val type: String,
--- app/src/main/java/com/dmm/bootcamp/yatter2024/infra/api/json/PostStatusJson.kt
+++ app/src/main/java/com/dmm/bootcamp/yatter2024/infra/api/json/PostStatusJson.kt
@@ -1,7 +1,9 @@
 package com.dmm.bootcamp.yatter2024.infra.api.json
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class PostStatusJson(
   val status: String,
   @Json(name = "media_ids") val mediaIds: List<Int>
