43e0cd55c4f5c019545a1e2f122a9f18c0e8fdfb
yt8492 <tomiyama-yuta2@dmm.com>
2024-06-04 16:42:12 +0900

    ViewModelModuleを資料の通りに修正

--- app/src/main/java/com/dmm/bootcamp/yatter2024/di/ViewModelModule.kt
+++ app/src/main/java/com/dmm/bootcamp/yatter2024/di/ViewModelModule.kt
@@ -3,6 +3,10 @@ package com.dmm.bootcamp.yatter2024.di
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-val viewModelModule = module {
+internal val viewModelModule = module {
+//  viewModel { MainViewModel(get()) }
 //  viewModel { PublicTimelineViewModel(get()) }
+//  viewModel { PostViewModel(get(), get()) }
+//  viewModel { RegisterAccountViewModel(get()) }
+//  viewModel { LoginViewModel(get()) }
 }
