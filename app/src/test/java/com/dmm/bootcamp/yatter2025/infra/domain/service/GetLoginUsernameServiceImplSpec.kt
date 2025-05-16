90b5f4fb29310bc50b2a25b48b1a91547267178f
hosoi-sota <hosoi-sota@dmm.com>
2025-05-16 18:33:20 +0900

    gradlewに実行権限を付与

--- app/src/test/java/com/dmm/bootcamp/yatter2025/infra/domain/service/GetLoginUsernameServiceImplSpec.kt
+++ app/src/test/java/com/dmm/bootcamp/yatter2025/infra/domain/service/GetLoginUsernameServiceImplSpec.kt
@@ -4,6 +4,7 @@ import com.dmm.bootcamp.yatter2025.domain.model.Username
 import com.dmm.bootcamp.yatter2025.infra.pref.LoginUserPreferences
 import com.google.common.truth.Truth.assertThat
 import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
@@ -16,18 +17,18 @@ class GetLoginUsernameServiceImplSpec {
   fun getLoginUsername() {
     val username = "username"
 
-    coEvery { loginUserPreferences.getUsername() } returns username
+    every { loginUserPreferences.getUsername() } returns username
 
-    val result = runBlocking { subject.execute() }
+    val result = subject.execute()
 
     assertThat(result).isEqualTo(Username(value = username))
   }
 
   @Test
   fun getLoginUsernameNull() {
-    coEvery { loginUserPreferences.getUsername() } returns null
+    every { loginUserPreferences.getUsername() } returns null
 
-    val result = runBlocking { subject.execute() }
+    val result = subject.execute()
 
     assertThat(result).isNull()
   }
